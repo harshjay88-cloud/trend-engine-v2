f5f1ffc0ecae5fda73f7e4526c9199382cf3526c
harshjay88-cloud <harshjay88@gmail.com>
2026-03-10 12:06:16 +0100

    Update trend_score.py

--- analyzers/trend_score.py
+++ analyzers/trend_score.py
@@ -1,1 +1,9 @@
+def trend_score(search_growth,velocity,competition):
 
+    if competition == 0:
+
+        competition = 1
+
+    score = (search_growth * velocity) / competition
+
+    return score
