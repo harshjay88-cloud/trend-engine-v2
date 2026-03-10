51ba44a2b73c1f096c9d101ce7136a0a5a788b42
harshjay88-cloud <harshjay88@gmail.com>
2026-03-10 12:06:57 +0100

    Update jobs.py

--- scheduler/jobs.py
+++ scheduler/jobs.py
@@ -1,1 +1,17 @@
+import schedule
+import time
+from collectors.youtube_suggest import get_suggestions
 
+def collect():
+
+    data = get_suggestions("music")
+
+    print(data)
+
+schedule.every(2).hours.do(collect)
+
+while True:
+
+    schedule.run_pending()
+
+    time.sleep(60)
