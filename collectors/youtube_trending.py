3cdd864ad898863e076b9b4a5b35a941d75c6f9b
harshjay88-cloud <harshjay88@gmail.com>
2026-03-10 12:04:58 +0100

    Update youtube_trending.py

--- collectors/youtube_trending.py
+++ collectors/youtube_trending.py
@@ -1,1 +1,17 @@
+import requests
 
+def get_trending_videos():
+
+    url = "https://www.googleapis.com/youtube/v3/videos"
+
+    params = {
+        "part": "snippet,statistics",
+        "chart": "mostPopular",
+        "regionCode": "US",
+        "maxResults": 25,
+        "key": "YOUR_API_KEY"
+    }
+
+    r = requests.get(url,params=params)
+
+    return r.json()
