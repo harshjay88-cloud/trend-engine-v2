305a80f2b17859057888300bb7ab633fcf8fbce3
harshjay88-cloud <harshjay88@gmail.com>
2026-03-10 12:05:41 +0100

    Update keyword_expander.py

--- engine/keyword_expander.py
+++ engine/keyword_expander.py
@@ -1,1 +1,19 @@
+modifiers = [
+"remix",
+"dj",
+"nonstop",
+"sad",
+"rap",
+"phonk",
+"2026"
+]
 
+def expand(keyword):
+
+    results = []
+
+    for m in modifiers:
+
+        results.append(f"{keyword} {m}")
+
+    return results
