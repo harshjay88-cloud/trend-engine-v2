10d3b0a7fa44558d91f0449bcfdab76c790c7577
harshjay88-cloud <harshjay88@gmail.com>
2026-03-10 12:08:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from collectors.youtube_suggest import get_suggestions
+from engine.keyword_expander import expand
 
+base = "music"
+
+keywords = get_suggestions(base)
+
+for k in keywords:
+
+    expanded = expand(k)
+
+    print("Base:",k)
+
+    print("Expanded:",expanded)
