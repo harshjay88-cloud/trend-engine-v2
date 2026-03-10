e39abeab284be3e93f42d67c1b0c4d3ba14029e5
harshjay88-cloud <harshjay88@gmail.com>
2026-03-10 12:03:18 +0100

    Update youtube_suggest.py

--- collectors/youtube_suggest.py
+++ collectors/youtube_suggest.py
@@ -1,1 +1,9 @@
+import requests
 
+def get_suggestions(query):
+
+    url = f"https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q={query}"
+
+    r = requests.get(url).json()
+
+    return r[1]
