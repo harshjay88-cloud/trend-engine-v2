15338a8fa2bc451630f32b10ec7da3862401aac3
harshjay88-cloud <harshjay88@gmail.com>
2026-03-10 12:06:36 +0100

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,1 +1,31 @@
+import sqlite3
 
+conn = sqlite3.connect("trend_engine.db")
+
+cursor = conn.cursor()
+
+cursor.execute("""
+
+CREATE TABLE IF NOT EXISTS trends(
+
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+keyword TEXT,
+
+score REAL
+
+)
+
+""")
+
+def save(keyword,score):
+
+    cursor.execute(
+
+        "INSERT INTO trends(keyword,score) VALUES(?,?)",
+
+        (keyword,score)
+
+    )
+
+    conn.commit()
